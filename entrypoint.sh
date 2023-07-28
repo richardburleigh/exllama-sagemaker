61329206e5fa1dcce029f2c70c0d53b942242565
Richard Burleigh <richard@richardburleigh.com.au>
2023-07-28 17:39:27 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,11 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
+#!/bin/bash
 
-# Ensure that the application state path is set
-if [ -z $APPLICATION_STATE_PATH ]; then
-  echo "Must specify application state path"
-  exit 1
+set -e
+
+if [ "$1" = 'serve' ]; then
+    echo "Starting server..."
+    python3 /app/server.py
+else
+    echo "Not implemented"
+    exit 1
 fi
-
-# Ensure that bind-mounted directories are owned by the user that runs the service if the user is not root
-if [ $RUN_UID -ne 0 ]; then
-  chown -R $RUN_UID:$RUN_UID $APPLICATION_STATE_PATH
-fi
-
-# Run service as specified (non-root) user
-exec runuser -u $(id -un $RUN_UID) -- python3 /app/webui/app.py \
-	-d $CONTAINER_MODEL_PATH \
-	--sessions_dir $CONTAINER_SESSIONS_PATH \
-	$@
